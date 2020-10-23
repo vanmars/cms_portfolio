63047b3d53e63a841d360d3611baa2d39146c6ce
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-23 16:01:12 -0700

    Enable only admins to create, update, and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
-
+  before_action except: [:index, :show] do 
+    redirect_to projects_path unless is_admin?
+  end
+  
   def index
     @posts = Post.all
     render :index
