c9df4918ae3934d0f4479aa85dd129a0c40b23c2
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-23 15:41:11 -0700

    Add helper method to check user is admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  # Ensures only logged in users can access routes
   before_action :authenticate_user!
+
+  # Helpers for Controller and Views
+  helper_method :is_admin
+
+  def is_admin?
+    current_user && current_user.is_admin
+  end
 end
