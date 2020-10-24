25c308bdfb68b8d8348a8aae10be401c5a10ccc4
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-23 23:04:06 -0700

    Write and PASS validation tests for comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
+  # Associations
   belongs_to :post
   belongs_to :user
+
+  # Validations
+  validates :body, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 describe Comment do
   it { should belong_to :post }
+  it { should belong_to :user }
+
+  it { should validate_presence_of :body }
 end
