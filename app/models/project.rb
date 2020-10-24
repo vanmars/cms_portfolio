ee58a79657cf2fe4963d7e7495acfd9572572d89
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-23 22:55:39 -0700

    Write and PASS validation tests for projects model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ApplicationRecord
+  # Active Storage Image
   has_one_attached :image
+
+  # Validations
+  validates :title, :description, :complete, presence: true
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 describe Project do
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :complete }
 end
