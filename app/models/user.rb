197bd0f488f0a1227f07b19d7528f5407e20b1e3
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-24 00:08:06 -0700

    Edit user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   # Validations
-  validates :first_name, :last_name, :username, :email, :password, presence: true
+  validates :email, :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 describe User do
   it { should have_many :comments }
 
-  it { should validate_presence_of :first_name}
-  it { should validate_presence_of :last_name}
-  it { should validate_presence_of :username}
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 end
