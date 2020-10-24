5591ebf6fa67d7555ad2a2c933ad8cf455055963
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-23 23:02:19 -0700

    Write and PASS validation tests for users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  # Associations
   has_many :comments, dependent: :destroy
+
+  # Validations
+  validates :first_name, :last_name, :username, :email, :password, :admin, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 describe User do
+  it { should have_many :comments }
+
+  it { should validate_presence_of :first_name}
+  it { should validate_presence_of :last_name}
+  it { should validate_presence_of :username}
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
+  it { should validate_presence_of :admin }
 end
