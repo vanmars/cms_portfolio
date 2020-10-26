8db1c4832f1b83498a6d406b7893d3363c3d8572
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-25 23:44:16 -0700

    Add and PASS test for sign up

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -4,21 +4,13 @@ describe 'register process with registered user' do
   it 'signs in a registered user' do
     visit '/'
     click_link 'Sign Up'
-    fill_in 'Email', with: 'test@email.com'
+    fill_in 'First name', with: 'Ada'
+    fill_in 'Last name', with: 'Lovelace'
+    fill_in 'Username', with: 'alovelace'
+    fill_in 'Email', with: 'spectest@email.com'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
-    click_on 'Sign Up'
+    click_on 'Sign up'
     expect(page).to have_content('Welcome')
   end
-end
-
-# describe 'register process with already registered user' do
-#   it 'signs in a registered user' do
-#     visit '/'
-#     click_link 'Sign In'
-#     fill_in 'Email', with: 'test20@email.com'
-#     fill_in 'Password', with: 'password'
-#     click_on 'Log in'
-#     expect(page).to have_content('Invalid')
-#   end
 end
