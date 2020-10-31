3fff4a09ca2c2fa2cbe3c7d7502c47ce6724f7a5
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-30 23:26:19 -0700

    Enable all fields to be saved to db upon registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,13 @@ class ApplicationController < ActionController::Base
   def is_creator?
     Comment.find(params[:id]).user == current_user
   end
+
+  # Configure Devise Strong Parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name])
+  end
 end
